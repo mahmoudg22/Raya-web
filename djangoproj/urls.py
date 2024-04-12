83915b702109f9a78b08fdd81f3ee82f3719143c
mahmoudg22 <m.hegazi2050@gmail.com>
2024-04-12 18:21:44 -0400

    Adding temporary changes to Github

--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from djangoapp.views import home_screen_view
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+   
+   path('admin/', admin.site.urls),
+   path('', home_screen_view),
+    
+  
+] 
